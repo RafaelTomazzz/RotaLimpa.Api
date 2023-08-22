fa4292735f0c948aa6e5fab8722a49ed6a9e1e77
Etec Prof. Horácio Augusto da Silveira <rafaeltomaz2701@gmail.com>
2023-08-21 21:23:50 -0300

    Consertei o erro no DataContext - Rafael

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using RotaLimpa.api.Models;
 
-/*namespace RotaLimpa.api.Data 
+namespace RotaLimpa.api.Data 
 {
-    public DataContext(DbContextOptions<DataContext> options) : base(options)
-    {}
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {}
 
-    public DbSet<Funcionario> Funcionarios {get; set;}
-}*/
+        public DbSet<Funcionario> Funcionarios {get; set;}
+    }
+}
