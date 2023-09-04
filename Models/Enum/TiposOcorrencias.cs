ee2100f444a36172392a3b1e7827dcec2ba03c68
Alitor004 <irmaosgomes1302@gmail.com>
2023-09-03 22:35:06 -0300

    TiposOcorrencias

---
+++ Models/Enum/TiposOcorrencias.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RotaLimpa.Api.Models.Enuns
+{
+    public enum TiposOcorrencias
+    {
+        Colisão=1,
+        Feita=2,
+        ArvoreCaida=3,
+        SemSaida=4
+    }
+}
