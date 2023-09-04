5810bf30ae167096a0acac8b307c514c3b9a92b2
Alitor004 <irmaosgomes1302@gmail.com>
2023-09-03 21:44:46 -0300

    Setores

--- Models/Setores.cs
+++ Models/Setores.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using RotaLimpa.api.Models;
 using RotaLimpa.api.Models.Enuns;
 
 namespace RotaLimpa.Api.Models
