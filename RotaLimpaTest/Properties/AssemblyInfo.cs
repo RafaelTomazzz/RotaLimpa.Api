db84461cb0d8079cf27a4ed40e336c2f65d51f6e
glennancy <glennancy24@gmail.com>
2023-11-05 13:08:21 -0300

    Tentando criar teste unitario

---
+++ RotaLimpaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RotaLimpaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RotaLimpaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f40607b3-80ec-499a-a177-b6c1adf19465")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RotaLimpaTest/RuasTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RotaLimpa.Api;
+
+namespace RotaLimpaTest
+{
+    [TestClass]
+    public class RuasTest
+    {
+        [TestMethod]
+        public void RuasDoesExiste()
+        {
+            RuasController rua = new RuasController();
+            bool GetSingle;
+
+            GetSingle = rua.GetSingle(1);
+
+            Assert.IsTrue(GetSingle);
+        }
+        
+        [TestMethod]
+        public void RuasDoesNotExiste()
+        {
+            //TODO;
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void RuasNullOrEmpty_Throw()
+        {
+            //TODO;
+            Assert.Inconclusive();
+        }
+    }
+}
