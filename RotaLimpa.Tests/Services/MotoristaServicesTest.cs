30a15d07fa83f0d4b07a4d11ee72d7063fe12374
Aluno <aluno@etec.sp.gov.br>
2023-11-17 22:44:31 -0300

    test: add tests for generate unique login

--- RotaLimpa.Tests/Services/MotoristaServicesTest.cs
+++ RotaLimpa.Tests/Services/MotoristaServicesTest.cs
@@ -1,11 +1,44 @@
-﻿namespace RotaLimpa.Tests.Services
+﻿using Moq;
+using RotaLimpa.Api.Repositories.Interfaces;
+using RotaLimpa.Api.Repositories.UnitOfWork;
+using RotaLimpa.Api.Services;
+
+namespace RotaLimpa.Tests.Services
 {
     public class MotoristaServicesTest
     {
-        [Fact]
-        public void MyTest()
+        private readonly Mock<IMotoristasRepository> _mockMotoristaRepository;
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly MotoristasService _service;
+
+
+        public MotoristaServicesTest()
         {
-            Assert.Equal(1, 1);
+            _mockMotoristaRepository = new Mock<IMotoristasRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _service = new MotoristasService(_mockMotoristaRepository.Object, _mockUnitOfWork.Object);
+        }
+
+        [Fact]
+        public async Task ShouldGenerateUniqueLoginInTheYearDifferentCurrentYear()
+        {
+            var expectedLogin = "0011123";
+            string loginAtual = await _service.GerarUnicoLoginAsync();
+
+            Assert.Equal(loginAtual, expectedLogin);
+        }
+
+        [Fact]
+        public async Task ShouldGenerateLogin999()
+        {
+            string loginAtual = "";
+            for (int i = 0; i < 3; i++)
+            {
+                loginAtual = await _service.GerarUnicoLoginAsync();
+
+            }
+            var expectedLogin = "0041123";
+            Assert.Equal(loginAtual, expectedLogin);
         }
     }
 }
