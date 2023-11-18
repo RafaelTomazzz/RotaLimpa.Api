70f24588a85f934e1c48af4276d15267112f3d72
Etec Prof. Horácio Augusto da Silveira <rafaeltomaz2701@gmail.com>
2023-11-17 22:26:46 -0300

    dto de motorista

---
+++ DTO/Builder/MotoristaDTOBuilder.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RotaLimpa.Api.DTO.Builder
+{
+    public class MotoristaDTOBuilder
+    {
+        private MotoristaDTO _motoristaDTO = new MotoristaDTO();
+
+        public MotoristaDTOBuilder WithId(int id)
+        {
+            _motoristaDTO.Id = id;
+            return this;
+        }
+
+        public MotoristaDTOBuilder WithPNome(string pnome)
+        {
+            _motoristaDTO.PNome = pnome;
+            return this;
+        }
+
+        public MotoristaDTOBuilder WithSNome(string snome)
+        {
+            _motoristaDTO.SNome = snome;
+            return this;
+        }
+
+        public MotoristaDTOBuilder WithCpf(string cpf)
+        {
+            _motoristaDTO.Cpf = cpf;
+            return this;
+        }
+
+        public MotoristaDTO Build()
+        {
+            return _motoristaDTO;
+        }
+    }
+}
---
+++ DTO/MotoristaDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RotaLimpa.Api.DTO
+{
+    public class MotoristaDTO
+    {
+        public int Id { get; set; }
+        public string PNome { get; set; }
+        public string SNome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
