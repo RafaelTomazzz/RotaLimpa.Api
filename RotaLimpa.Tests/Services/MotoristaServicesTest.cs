509407de1f571cfe0ffca90307c6d7bf05d951c1
Aluno <aluno@etec.sp.gov.br>
2023-11-17 22:27:08 -0300

    test: add motorista tests

---
+++ RotaLimpa.Tests/Services/MotoristaServicesTest.cs
@@ -0,0 +1,11 @@
+﻿namespace RotaLimpa.Tests.Services
+{
+    public class MotoristaServicesTest
+    {
+        [Fact]
+        public void MyTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
